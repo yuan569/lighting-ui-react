e9e33636c46c697d99a13284e583368213c02222
yuan569 <yuan569@163.com>
2024-04-04 08:42:27 +0800

    fix bugs

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import Button, { ButtonProps, ButtonType, ButtonSize } from './button'
+import Button, { ButtonProps } from './button'
 
 
 
