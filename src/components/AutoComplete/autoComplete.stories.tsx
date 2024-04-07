618a4c121bf5c0db52e7f83a5f41f659888ab2b1
yuan569 <yuan569@163.com>
2024-04-08 02:57:18 +0800

    格式化文件

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { AutoComplete } from './autoComplete'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { AutoComplete } from "./autoComplete";
 
 const SimpleComplete = () => {
   // const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
   // 'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
   const lakersWithNumber = [
-    { value: 'bradley', number: 11 },
-    { value: 'pope', number: 1 },
-    { value: 'caruso', number: 4 },
-    { value: 'cook', number: 2 },
-    { value: 'cousins', number: 15 },
-    { value: 'james', number: 23 },
-    { value: 'AD', number: 3 },
-    { value: 'green', number: 14 },
-    { value: 'howard', number: 39 },
-    { value: 'kuzma', number: 0 },
-  ]
+    { value: "bradley", number: 11 },
+    { value: "pope", number: 1 },
+    { value: "caruso", number: 4 },
+    { value: "cook", number: 2 },
+    { value: "cousins", number: 15 },
+    { value: "james", number: 23 },
+    { value: "AD", number: 3 },
+    { value: "green", number: 14 },
+    { value: "howard", number: 39 },
+    { value: "kuzma", number: 0 },
+  ];
   // const handleFetch = (query: string) => {
   //   return lakers.filter(name => name.includes(query)).map(name => ({value: name}))
   // }
   const handleFetch = (query: string) => {
-    return lakersWithNumber.filter(player => player.value.includes(query))
-  }
+    return lakersWithNumber.filter((player) => player.value.includes(query));
+  };
   //   const handleFetch = (query: string) => {
   //     return fetch(`https://api.github.com/search/users?q=${query}`)
   //       .then(res => res.json())
@@ -43,16 +43,14 @@ const SimpleComplete = () => {
   //   )
   // }
   return (
-    <div style={{ 'width': '260px' }}>
+    <div style={{ width: "260px" }}>
       <AutoComplete
         fetchSuggestions={handleFetch}
-        onSelect={action('selected')}
-      //renderOption={renderOption}
+        onSelect={action("selected")}
+        //renderOption={renderOption}
       />
     </div>
+  );
+};
 
-  )
-}
-
-storiesOf('AutoComplete', module)
-  .add('自动补全输入框', SimpleComplete)
+storiesOf("AutoComplete", module).add("自动补全输入框", SimpleComplete);
