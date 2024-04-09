213d284c10cb428bd212874190c744a52e73e4d6
yuan569 <yuan569@163.com>
2024-04-10 02:41:24 +0800

    format some file

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,20 +1,29 @@
-import React, { FC, useState, ChangeEvent, KeyboardEvent, ReactElement, useEffect, useRef } from 'react'
-import classNames from 'classnames'
-import Input, { InputProps } from '../Input/input'
-import Icon from '../Icon/icon'
-import Transition from '../Transition/transition'
-import useDebounce from '../../hooks/useDebounce'
-import useClickOutside from '../../hooks/useClickOutside'
+import React, {
+  FC,
+  useState,
+  ChangeEvent,
+  KeyboardEvent,
+  ReactElement,
+  useEffect,
+  useRef,
+} from "react";
+import classNames from "classnames";
+import Input, { InputProps } from "../Input/input";
+import Icon from "../Icon/icon";
+import Transition from "../Transition/transition";
+import useDebounce from "../../hooks/useDebounce";
+import useClickOutside from "../../hooks/useClickOutside";
 interface DataSourceObject {
   value: string;
 }
-export type DataSourceType<T = {}> = T & DataSourceObject
-export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => DataSourceType[] | Promise<DataSourceType[]>;
+export type DataSourceType<T = {}> = T & DataSourceObject;
+export interface AutoCompleteProps extends Omit<InputProps, "onSelect"> {
+  fetchSuggestions: (
+    str: string
+  ) => DataSourceType[] | Promise<DataSourceType[]>;
   onSelect?: (item: DataSourceType) => void;
   renderOption?: (item: DataSourceType) => ReactElement;
 }
-
 
 /**
  * ### 引用方法
@@ -22,123 +31,126 @@ export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
  * ~~~js
  * import { AutoComplete } from 'lighting-ui-react'
  * ~~~
- * @param props 
+ * @param props
  */
 
 export const AutoComplete: FC<AutoCompleteProps> = (props) => {
-  const {
-    fetchSuggestions,
-    onSelect,
-    value,
-    renderOption,
-    ...restProps
-  } = props
+  const { fetchSuggestions, onSelect, value, renderOption, ...restProps } =
+    props;
 
-  const [ inputValue, setInputValue ] = useState(value as string)
-  const [ suggestions, setSugestions ] = useState<DataSourceType[]>([])
-  const [ loading, setLoading ] = useState(false)
-  const [ showDropdown, setShowDropdown] = useState(false)
-  const [ highlightIndex, setHighlightIndex] = useState(-1)
-  const triggerSearch = useRef(false)
-  const componentRef = useRef<HTMLDivElement>(null)
-  const debouncedValue = useDebounce(inputValue, 300)
-  useClickOutside(componentRef, () => { setSugestions([])})
+  const [inputValue, setInputValue] = useState(value as string);
+  const [suggestions, setSugestions] = useState<DataSourceType[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [highlightIndex, setHighlightIndex] = useState(-1);
+  const triggerSearch = useRef(false);
+  const componentRef = useRef<HTMLDivElement>(null);
+  const debouncedValue = useDebounce(inputValue, 300);
+  useClickOutside(componentRef, () => {
+    setSugestions([]);
+  });
   useEffect(() => {
     if (debouncedValue && triggerSearch.current) {
-      setSugestions([])
-      const results = fetchSuggestions(debouncedValue)
+      setSugestions([]);
+      const results = fetchSuggestions(debouncedValue);
       if (results instanceof Promise) {
-        setLoading(true)
-        results.then(data => {
-          setLoading(false)
-          setSugestions(data)
+        setLoading(true);
+        results.then((data) => {
+          setLoading(false);
+          setSugestions(data);
           if (data.length > 0) {
-            setShowDropdown(true)
+            setShowDropdown(true);
           }
-        })
+        });
       } else {
-        setSugestions(results)
-        setShowDropdown(true)
+        setSugestions(results);
+        setShowDropdown(true);
         if (results.length > 0) {
-          setShowDropdown(true)
-        } 
+          setShowDropdown(true);
+        }
       }
     } else {
-      setShowDropdown(false)
+      setShowDropdown(false);
     }
-    setHighlightIndex(-1)
-  }, [debouncedValue, fetchSuggestions])
+    setHighlightIndex(-1);
+  }, [debouncedValue, fetchSuggestions]);
   const highlight = (index: number) => {
-    if (index < 0) index = 0
+    if (index < 0) index = 0;
     if (index >= suggestions.length) {
-      index = suggestions.length - 1
+      index = suggestions.length - 1;
     }
-    setHighlightIndex(index)
-  }
+    setHighlightIndex(index);
+  };
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    switch(e.keyCode) {
+    switch (e.keyCode) {
       case 13:
         if (suggestions[highlightIndex]) {
-          handleSelect(suggestions[highlightIndex])
+          handleSelect(suggestions[highlightIndex]);
         }
-        break
+        break;
       case 38:
-        highlight(highlightIndex - 1)
-        break
+        highlight(highlightIndex - 1);
+        break;
       case 40:
-        highlight(highlightIndex + 1)
-        break
+        highlight(highlightIndex + 1);
+        break;
       case 27:
-        setShowDropdown(false)
-        break
+        setShowDropdown(false);
+        break;
       default:
-        break
+        break;
     }
-  }
+  };
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.replace(/^\s*|\s*$/g, '');
-    setInputValue(value)
-    triggerSearch.current = true
-  }
+    const value = e.target.value.replace(/^\s*|\s*$/g, "");
+    setInputValue(value);
+    triggerSearch.current = true;
+  };
   const handleSelect = (item: DataSourceType) => {
-    setInputValue(item.value)
-    setShowDropdown(false)
+    setInputValue(item.value);
+    setShowDropdown(false);
     if (onSelect) {
-      onSelect(item)
+      onSelect(item);
     }
-    triggerSearch.current = false
-  }
+    triggerSearch.current = false;
+  };
   const renderTemplate = (item: DataSourceType) => {
-    return renderOption ? renderOption(item) : item.value
-  }
+    return renderOption ? renderOption(item) : item.value;
+  };
   const generateDropdown = () => {
     return (
       <Transition
         in={showDropdown || loading}
         animation="zoom-in-top"
         timeout={300}
-        onExited={() => {setSugestions([])}}
+        onExited={() => {
+          setSugestions([]);
+        }}
       >
         <ul className="lighting-suggestion-list">
-          { loading &&
+          {loading && (
             <div className="suggstions-loading-icon">
-              <Icon name="loader"/>
+              <Icon name="loader" />
             </div>
-          }
+          )}
           {suggestions.map((item, index) => {
-            const cnames = classNames('suggestion-item', {
-              'is-active': index === highlightIndex
-            })
+            const cnames = classNames("suggestion-item", {
+              "is-active": index === highlightIndex,
+            });
             return (
-              <li key={index} className={cnames} onClick={() => handleSelect(item)}>
+              <li
+                key={index}
+                className={cnames}
+                onClick={() => handleSelect(item)}
+              >
                 {renderTemplate(item)}
               </li>
-            )
+            );
           })}
         </ul>
       </Transition>
-    )
-  }
+    );
+  };
   return (
     <div className="lighting-auto-complete" ref={componentRef}>
       <Input
@@ -149,8 +161,7 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
       />
       {suggestions && suggestions.length > 0 ? generateDropdown() : null}
     </div>
-  )
-}
+  );
+};
 
 export default AutoComplete;
-
