dfb87a9e91fc0e2b77826f695a0f411ccdec0480
yuan569 <yuan569@163.com>
2024-04-10 02:44:05 +0800

    format some file

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -1,26 +1,35 @@
-import React, {FC} from 'react'
-import classNames from 'classnames'
+import React, { FC } from "react";
+import classNames from "classnames";
 
-import { Icon as FeatherIcon, IconComponentProps as FeatherIconProps } from '@ailibs/feather-react-ts';
+import {
+  Icon as FeatherIcon,
+  IconComponentProps as FeatherIconProps,
+} from "@ailibs/feather-react-ts";
 
-
-export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
+export type ThemeProps =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "info"
+  | "warning"
+  | "danger"
+  | "light"
+  | "dark";
 
 export interface IconProps extends FeatherIconProps {
-    /** 设置主题色 */
-    theme?: ThemeProps,
-    /** 设置图标尺寸 */
-    size?: string,
-    /** 设置图标粗细 */
-    strokeWidth?: string,
+  /** 设置主题色 */
+  theme?: ThemeProps;
+  /** 设置图标尺寸 */
+  size?: string;
+  /** 设置图标粗细 */
+  strokeWidth?: string;
 }
-
 
 /**
  * * 提供了 280+ 常用的 icon 图标
  * * 基于开源图标库 [Feather-Icons](https://feathericons.com/) 封装。文档地址：[NPM 包](https://www.npmjs.com/package/@ailibs/feather-react-ts)
  * * 给组件设置 name 属性即可。
- * 
+ *
  * ### 引用方法
  * ---
  * ~~~js
@@ -29,27 +38,17 @@ export interface IconProps extends FeatherIconProps {
  */
 
 export const Icon: FC<IconProps> = (props) => {
-    const {
-        className,
-        theme,
-        ...restProps
-    } = props
+  const { className, theme, ...restProps } = props;
 
-    const classes = classNames('lighting-icon', className, {
-        [`icon-${theme}`]: theme,
-
-    })
-    return (
-        <FeatherIcon
-            className={classes}
-            {...restProps}
-        />
-    )
-}
+  const classes = classNames("lighting-icon", className, {
+    [`icon-${theme}`]: theme,
+  });
+  return <FeatherIcon className={classes} {...restProps} />;
+};
 
 Icon.defaultProps = {
-    size: '24',
-    strokeWidth: '2',
-}
+  size: "24",
+  strokeWidth: "2",
+};
 
 export default Icon;
